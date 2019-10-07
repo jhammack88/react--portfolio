778804c427f81a236568b64cd2ebdf1dd04affd8
jhammack88 <johnhammack88@gmail.com>
2019-10-07 16:26:04 -0600

    Updated Commit

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,24 +1,42 @@
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 export default class NavigationComponent extends Component {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    render() {
-        return (
-            <div>
-                <NavLink exact to="/" activeClassName="nav-link-active">
-                    Home
-                </NavLink>
+  render() {
+    return (
+      <div className="nav-wrapper">
+        <div className="left-side">
+          <div className="nav-link-wrapper">
+            <NavLink exact to="/" activeClassName="nav-link-active">
+              Home
+            </NavLink>
+          </div>
 
-                <NavLink to="/about-me"activeClassName="nav-link-active">About</NavLink>   
-                <NavLink to="/contact"activeClassName="nav-link-active">Contact</NavLink>
-                <NavLink to="/blog"activeClassName="nav-link-active">Blog</NavLink> 
+          <div className="nav-link-wrapper">
+            <NavLink to="/about-me" activeClassName="nav-link-active">
+              About
+            </NavLink>
+          </div>
 
-                {false ? <button>Add Blog</button> : null }
-            </div>
-        )
-    }
+          <div className="nav-link-wrapper">
+            <NavLink to="/contact" activeClassName="nav-link-active">
+              Contact
+            </NavLink>
+          </div>
+
+          <div className="nav-link-wrapper">
+            <NavLink to="/blog" activeClassName="nav-link-active">
+              Blog
+            </NavLink>
+          </div>
+        </div>
+
+        <div className="right-side">JOHN HAMMACK</div>
+      </div>
+    );
+  }
 }
--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -3,70 +3,69 @@ import axios from "axios";
 
 import PortfolioItem from "./portfolio-items";
 
-
 export default class PortfolioContainer extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            pageTitle: "Welcome to my portfolio", 
-            isLoading: false,
-            data: []
-        };
+    this.state = {
+      pageTitle: "Welcome to my portfolio",
+      isLoading: false,
+      data: []
+    };
 
-        this.handleFiter = this.handleFiter.bind(this);
-        this.getPortfolioItems = this.getPortfolioItems.bind(this);
-    }
+    this.handleFilter = this.handleFilter.bind(this);
+  }
 
-    getPortfolioItems() {
-        axios
-          .get('https://johnhammack.devcamp.space/portfolio/portfolio_items')
-          .then(response => {
-            this.setState({
-                data: response.data.portfolio_items
-            })
-          })
-          .catch(error => {
-            console.log(error);
-          })
-      }
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter(item => {
+        return item.category === filter;
+      })
+    });
+  }
 
-    portfolioItems() {
-        return this.state.data.map(item => {
-            // debugger;
-            return <PortfolioItem key={item.name} item={item} title={item.name} url= {item.url} slug={item.id} />;
-
-        })
-    }
-
-    handleFiter = filter => {
+  getPortfolioItems() {
+    axios
+      .get("https://jordan.devcamp.space/portfolio/portfolio_items")
+      .then(response => {
         this.setState({
-            data: this.state.data.filter(item => {
-                return item.category === filter;
-            })
-        })
+          data: response.data.portfolio_items
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  portfolioItems() {
+    return this.state.data.map(item => {
+      return <PortfolioItem key={item.id} item={item} />;
+    });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
     }
 
-    componentDidMount() {
-        this.getPortfolioItems();
-    }
+    return (
+      <div className="portfolio-items-wrapper">
+        <button className="btn" onClick={() => this.handleFilter("eCommerce")}>
+          eCommerce
+        </button>
+        <button className="btn" onClick={() => this.handleFilter("Scheduling")}>
+          Scheduling
+        </button>
+        <button className="btn" onClick={() => this.handleFilter("Enterprise")}>
+          Enterprise
+        </button>
 
-    render() {
-        if (this.state.isLoading) {
-            return <div>Loading...</div>;
-        }
-
-
-        return (
-            <div>
-                <h2>{this.state.pageTitle}</h2>
-
-                <button onClick={() => this.handleFilter('ecommerce')}>eCommerce</button>
-                <button onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
-                <button onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
-
-                {this.portfolioItems()}
-            </div>
-        )
-    }
+        {this.portfolioItems()}
+      </div>
+    );
+  }
 }
