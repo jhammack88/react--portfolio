0cd40448b3c6e8b3eb42a95e611f24eda988537f
jhammack88 <johnhammack88@gmail.com>
2019-10-28 11:23:57 -0600

    python buildpack removed

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -16,7 +16,7 @@ export default class PortfolioForm extends Component {
       banner_image: "",
       logo: "",
       editMode: false,
-      apiUrl: "https://jordan.devcamp.space/portfolio/portfolio_items",
+      apiUrl: "https://johnhammack.devcamp.space/portfolio/portfolio_items",
       apiAction: "post"
     };
 
@@ -75,7 +75,7 @@ export default class PortfolioForm extends Component {
         position: position || "",
         url: url || "",
         editMode: true,
-        apiUrl: `https://jordan.devcamp.space/portfolio/portfolio_items/${id}`,
+        apiUrl: `https://johnhammack.devcamp.space/portfolio/portfolio_items/${id}`,
         apiAction: "patch",
         thumb_image_url: thumb_image_url || "",
         banner_image_url: banner_image_url || "",
